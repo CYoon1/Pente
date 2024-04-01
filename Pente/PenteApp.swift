d3e613a569f13ccf7ee83938ec68d7937117da59
CYoon1 <68024575+CYoon1@users.noreply.github.com>
2024-04-01 18:35:51 -0400

    Initial Commit

---
+++ Pente/PenteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PenteApp.swift
+//  Pente
+//
+//  Created by Christopher Yoon on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct PenteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
